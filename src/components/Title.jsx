55b98b972cfc7c6f8944d9085b089a184fe01c25
Victor Matias <fvictor1705@gmail.com>
2022-07-20 16:06:18 -0300

    Resolve requisito 4.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
 
-    return <h2>{headline}</h2>;
+    return <h2>{ headline }</h2>;
   }
 }
 
 export default Title;
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
