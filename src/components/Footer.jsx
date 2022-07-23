d995b811f8daebab1505b51c66c9355bc0e28349
Victor Matias <fvictor1705@gmail.com>
2022-07-22 21:47:04 -0300

    Adiciona componente Footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          Um site feito por
+          <a href="https://victormatia.github.io/"> Victor Matias</a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
